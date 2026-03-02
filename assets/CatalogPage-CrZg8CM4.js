9e391fdefc5ca9e42b48b168cc426a466f498370
artemstadnik <stadnikartem1709@gmail.com>
2026-03-02 16:56:00 +0000

    Deploying to gh-pages from @ artemstadnik/books-catalog@cce82f58463d756d112bc8f1d41d7c7cb1d79801 🚀

---
+++ assets/CatalogPage-CrZg8CM4.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-Dyd3TlEA.js";const a=()=>o.jsx("div",{children:"Catalog"});export{a as CatalogPage};
---
+++ assets/useUserOrders-BDbG_xqp.js
@@ -0,0 +1,1 @@
+import{o as r}from"./index-Dyd3TlEA.js";import{d as e}from"./paymentAPI-BLJcUnk_.js";const u=()=>r({queryKey:["orders","user"],queryFn:e});export{u};
